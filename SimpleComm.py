ca3a719c96248c55d9dcce77c8cbcfed7c2e9ecd
JoanVC <36604048+JoanVC100@users.noreply.github.com>
2024-09-19 10:25:50 +0200

    Add async methods for asyncio streams and aioserial

--- SimpleComm.py
+++ SimpleComm.py
@@ -1,3 +1,10 @@
+has_aioserial = False
+try:
+	import aioserial
+	has_aioserial = True
+except ModuleNotFoundError:
+	pass
+
 class SimplePacket:
 	def __init__(self):
 		self.source = 0
@@ -73,6 +80,30 @@ class SimpleComm:
 	def getAddress():
 		return SimpleComm.address
 
+	async def async_send(stream, packet, destination = None, type = None):
+		packet.setSource(SimpleComm.getAddress())
+
+		if destination != None:
+			packet.setDestination(destination)
+
+		if type != None:
+			packet.setType(type)
+
+		data = packet.getData()
+		buffer = bytearray()
+		buffer.append(SimpleComm.SYN)
+		buffer.append(len(data) + 4)
+		buffer.append(packet.getDestination())
+		buffer.append(packet.getSource())
+		buffer.append(packet.getType())
+		buffer += data
+		buffer.append(SimpleComm.calcCRC(buffer[2:]))
+
+		if has_aioserial and isinstance(stream, aioserial.Serial):
+			await stream.write_async(bytes(buffer))
+		else:
+			stream.write(bytes(buffer))
+
 	def send(stream, packet, destination = None, type = None):
 		packet.setSource(SimpleComm.getAddress())
 
@@ -93,6 +124,49 @@ class SimpleComm:
 		buffer.append(SimpleComm.calcCRC(buffer[2:]))
 
 		stream.write(bytes(buffer))
+
+	async def async_receive(stream):
+		if isinstance(stream, aioserial.Serial):
+			stream_read_fun = stream.read_async
+		else:
+			stream_read_fun = stream.read
+		try:
+			while True:
+				r = await stream_read_fun(1)
+				if len(r) == 0:
+					break
+
+				if r[0] != SimpleComm.SYN:
+					continue
+
+				r = await stream_read_fun(1)
+				if len(r) == 0:
+					break
+
+				tlen = r[0]
+				if tlen < 4:
+					continue
+
+				buffer = await stream_read_fun(tlen)
+				if len(buffer) != tlen:
+					break;
+
+				crc = SimpleComm.calcCRC(buffer[0:-1])
+				if crc != buffer[-1]:
+					continue
+
+				packet = SimplePacket()
+				packet.setDestination(buffer[0])
+				packet.setSource(buffer[1])
+				packet.setType(buffer[2])
+				packet.setData(buffer[3:-1])
+
+				return packet
+
+		except:
+			return None
+
+		return None
 
 	def receive(stream):
 		try:
